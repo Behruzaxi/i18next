78b73577bc9074d09db54c61272c6761c4e4e415
Behruzaxi <ebehruz846@gmail.com>
2023-04-07 15:48:11 +0500

    gfd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import i18next from "i18next"
+import "./index.css"
+import { useTranslation } from 'react-i18next'
 
-function App() {
+
+export default function App() {
+
+  const { t } = useTranslation()
+
+  const handleClick = (lang) => {
+    i18next.changeLanguage(lang)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   <>
 
-export default App;
+
+    <div className='buttons'>
+    <button className='buto'   onClick={() => handleClick("eng")}>English</button>
+   <button className='buto'  onClick={() => handleClick("ru")}>Russian</button>
+   <button className='buto'   onClick={() => handleClick("uz")}>Uzbek</button>
+    </div>
+    
+    
+    <h1 className='soz'>{t("text.text_paragraf")}</h1>
+   </>
+  )
+}
